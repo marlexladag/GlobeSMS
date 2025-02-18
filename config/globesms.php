25b657ad760466cf51b2df06f455c0b8a5a0a689
marlex <marlexlopasladag@gmail.com>
2025-02-18 21:56:37 +0800

    feat: add config

--- config/globesms.php
+++ config/globesms.php
@@ -2,5 +2,7 @@
 
 // config for MarlexLadag/GlobeSMS
 return [
-
+    'short_code' => env('GLOBE_SMS_SHORT_CODE', ''),
+    'app_id' => env('GLOBE_SMS_APP_ID', ''),
+    'app_secret' => env('GLOBE_SMS_APP_SECRET', ''),
 ];
